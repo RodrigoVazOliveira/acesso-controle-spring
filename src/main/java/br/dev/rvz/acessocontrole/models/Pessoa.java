19e37a03937dbf4412c492b8f4790e9b460b6b61
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 15:38:12 -0300

    Modificar classe Pessoa para modelo bean

--- src/main/java/br/dev/rvz/acessocontrole/models/Pessoa.java
+++ src/main/java/br/dev/rvz/acessocontrole/models/Pessoa.java
@@ -9,6 +9,46 @@ public class Pessoa {
     private String senha;
     private Endereco endereco;
 
+    public Pessoa() {
+    }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
