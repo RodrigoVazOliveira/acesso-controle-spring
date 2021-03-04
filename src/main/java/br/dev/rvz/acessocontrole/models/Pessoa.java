d25ce5d68da951c0eec777f074701fd1f6fe1586
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 15:36:47 -0300

    Criar modelo de Pessoa

---
+++ src/main/java/br/dev/rvz/acessocontrole/models/Pessoa.java
@@ -0,0 +1,14 @@
+package br.dev.rvz.acessocontrole.models;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String username;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String senha;
+    private Endereco endereco;
+
+
+
+}
