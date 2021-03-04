feb30d7931ac5dc3245ab1517f082ac9fc487d69
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 15:35:08 -0300

    Criar projeto incial com spring boot

---
+++ src/main/java/br/dev/rvz/acessocontrole/AcessoControleApplication.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.acessocontrole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoControleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoControleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/rvz/acessocontrole/AcessoControleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rvz.acessocontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoControleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
