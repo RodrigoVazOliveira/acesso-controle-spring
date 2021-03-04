18d449035b64f1e0316d325f5f3bdba6f620de43
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-03-04 15:37:38 -0300

    Criar modelo de Endereço

---
+++ src/main/java/br/dev/rvz/acessocontrole/models/Endereco.java
@@ -0,0 +1,44 @@
+package br.dev.rvz.acessocontrole.models;
+
+public class Endereco {
+
+    private String rua;
+    private String numero;
+    private String cidade;
+    private String cep;
+
+    public Endereco() {
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
